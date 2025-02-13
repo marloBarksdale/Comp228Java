e816ee9d0bacd9e40635472010b05db967ffb281
marloBarksdale <lyndenflood@gmail.com>
2025-02-13 15:28:24 -0500

    exercise 3 added

--- Lab 3/src/exercise3/Mortgage.java
+++ Lab 3/src/exercise3/Mortgage.java
@@ -1,29 +1,77 @@
 package exercise3;
 
-public class Mortgage {
+public abstract class Mortgage implements MortgageConstants {
 
 
-
-    int mortgageNumber;
-    String customerName;
-    double mortgageAmount;
-    int mortgageTerm;
-    double interestRate;
-
+    private int mortgageNumber;
+    private String customerName;
+    private double mortgageAmount;
+    private int mortgageTerm;
+    private double interestRate;
 
 
+    public Mortgage(int mortgageNumber, String customerName, double mortgageAmount, int mortgageTerm, double interestRate) {
+        this.mortgageNumber = mortgageNumber;
+        this.customerName = customerName;
+        this.mortgageAmount = mortgageAmount;
+        this.mortgageTerm = mortgageTerm;
+        this.interestRate = interestRate;
+    }
 
+    public int getMortgageNumber() {
+        return mortgageNumber;
+    }
 
+    public void setMortgageNumber(int mortgageNumber) {
+        this.mortgageNumber = mortgageNumber;
+    }
 
+    public String getCustomerName() {
+        return customerName;
+    }
 
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
+    public double getMortgageAmount() {
+        return mortgageAmount;
+    }
 
+    public void setMortgageAmount(double mortgageAmount) {
+        if (mortgageAmount > MAX_MORTGAGE_AMOUNT) {
+            System.out.printf("$%.2f exceeds the max amount of $%.2f. You mortgage will be set to the maximum.\n", mortgageAmount, MAX_MORTGAGE_AMOUNT);
+            this.mortgageAmount = MAX_MORTGAGE_AMOUNT;
+        } else {
+            this.mortgageAmount = mortgageAmount;
+        }
 
+    }
 
+    public int getMortgageTerm() {
+        return mortgageTerm;
+    }
 
+    public void setMortgageTerm(int mortgageTerm) {
 
+        if (mortgageTerm != SHORT_TERM && mortgageTerm != MEDIUM_TERM && mortgageTerm != LONG_TERM) {
+            System.out.println("Invalid term. Setting to short-term (1 year).");
+            this.mortgageTerm = SHORT_TERM;
+        } else {
+            this.mortgageTerm = mortgageTerm;
+        }
+    }
 
+    public double getInterestRate() {
+        return interestRate;
+    }
 
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
 
+    public String toString() {
 
+        String.format("")
+    }
 }
